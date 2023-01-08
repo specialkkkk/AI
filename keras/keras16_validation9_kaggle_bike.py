4f39e8fcb648f14287ffa7bbe179f0740ea76680
specialkkkk <121652193+specialkkkk@users.noreply.github.com>
2023-01-08 17:51:59 +0900

    Update keras16_validation9_kaggle_bike.py

--- keras/keras16_validation9_kaggle_bike.py
+++ keras/keras16_validation9_kaggle_bike.py
@@ -1,116 +1,70 @@
-import numpy as np
-import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_squared_log_error, r2_score
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
+import pandas as pd
+import numpy as np
 
 #1. 데이터
-path = './_data/bike/'
-train_csv = pd.read_csv(path + 'train.csv', index_col=0)
-test_csv = pd.read_csv(path + 'test.csv', index_col=0)
-submission = pd.read_csv(path + 'samplesubmission.csv', index_col=0)
+path = './keras_data/bike/'
+train_data = pd.read_csv(path + 'train.csv', index_col = 0)         # index_col = 0 → date_t 열 데이터로 취급 X
+test_data = pd.read_csv(path + 'test.csv', index_col = 0)
+submission = pd.read_csv(path + 'sampleSubmission.csv', index_col = 0)
 
-print(train_csv)
-print(train_csv.shape)  # (10886, 11)    #
+# print(train_data.shape)          # (10886, 11) 
+# print(test_data.shape)           # (6493, 8)
+# print(train_data.columns)   
+# print(train_data.info())         # Missing Attribute Values: 결측치 - 데이터에 값이 없는 것
+# print(train_data.describe())   # 평균, 표준편차, 최대값 등
 
+#  shape 맞추기 (열 제거) #
+train_data = train_data.drop(['casual', 'registered'], axis = 1)
 
-print(train_csv.columns)
+#  x,y 분리 #
+x = train_data.drop(['count'], axis=1)                              # y 값(count 열) 분리, axis = 1 → 열에 대해 동작
+y = train_data['count']                                             # y 값(count 열)만 추출
 
-print(train_csv.info())
-print(test_csv.info())
-print(train_csv.describe())
+x_train, x_test, y_train, y_test = train_test_split(
+    x,y,
+    train_size=0.7,
+    random_state=123
+)
 
-
-
-##### 결측치 처리 1. 제거 #####
-print(train_csv.isnull().sum())
-train_csv = train_csv.dropna()
-print(train_csv.shape)
-
-
-
-
-x = train_csv.drop(['count','casual','registered'], axis=1)
-print(x)   # [10886 rows x 10 columns]
-y = train_csv['count']
-print(y)
-print(y.shape) # (10886,)
-
-
-
-
-x_train, x_test, y_train, y_test = train_test_split(x, y,
-    train_size=0.7, shuffle=True, random_state=209)
-
-print(x_train.shape, x_test.shape)   # (7620, 10) (3266, 10)
-print(y_train.shape, y_test.shape)   # (7620,) (3266,)
-
-
-#2. 모델구성
+#2. 모델 구성
 model = Sequential()
-model.add(Dense(100, input_dim=8, activation='relu'))
-model.add(Dense(80, activation='relu'))
-model.add(Dense(60, activation='relu'))
-model.add(Dense(70, activation='relu'))
-model.add(Dense(80, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(80, activation='relu'))
-model.add(Dense(70, activation='relu'))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(1, activation='linear'))
+model.add(Dense(32, input_dim = 8))
+model.add(Dense(200, activation='relu'))
+model.add(Dense(500, activation='relu'))
+model.add(Dense(125, activation='relu'))
+model.add(Dense(14, activation='relu'))
+model.add(Dense(1))
+
+#3. 컴파일 및 훈련
+model.compile(loss='mse', optimizer='adam')
+start = t.time()
+model.fit(x_train, y_train, epochs = 100, batch_size=10, validation_split=0.2)
+fin = t.time()
 
 
 
-
-#3. 컴파일, 훈련
-import time
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-start= time.time()
-model.fit(x_train, y_train, epochs=200, batch_size=10)
-end = time.time()
-#배치 32  149
-
-
-
-
-#4. 평가,예측
-loss = model.evaluate(x_test, y_test)
-print('loss : ', loss)
+#4. 평가 및 예측
+loss = model.evaluate(x_test, y_test) 
+print('loss: ', loss)
 
 y_predict = model.predict(x_test)
-print(y_predict)
+# print('x_test:\n', x_test)
+# print('y_predict:\n', y_predict)
 
-
-
-
-def RMSE(y_test, y_predict):                                      # #predict : 예측값 y_test  y나눠진값.
-    return np.sqrt(mean_squared_error(y_test, y_predict))
-    
-print("RMSE:", RMSE(y_test, y_predict))
+RMSE = np.sqrt(mean_squared_error(y_test, y_predict))
+print("RMSE: ", RMSE)
+RMSLE = np.sqrt(mean_squared_log_error(y_test, y_predict))
+print("RMSLE: ", RMSLE)
 r2 = r2_score(y_test, y_predict)
-print("R2:", r2)
+print("R2: ", r2)
 
+print("소요시간: ", fin - start)
 
-
-#제출할 파일(submit)
-y_submit = model.predict(test_csv)
-print(y_submit)
-print(y_submit.shape)  # (715, 1)
-
-
-
-
-
+# 제출
+y_submit = model.predict(test_data)
 submission['count'] = y_submit
-submission.to_csv(path + 'submission_20230106_1.csv')
-print(submission)
-
-
-print("RMSE : ", RMSE(y_test, y_predict))
-
-print("걸린시간 : ", end - start)
-
-
-
-# 150.
+submission.to_csv(path + 'submission_0108.csv')
