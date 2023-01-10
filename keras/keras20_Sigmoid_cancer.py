4a90d0ca21e8fc582901e4ef71072354307e154f
Taemin Choi <specialkkkk@naver.com>
2023-01-10 09:29:53 +0900

    2023 01 10

--- keras/keras20_Sigmoid_cancer.py
+++ keras/keras20_Sigmoid_cancer.py
@@ -11,7 +11,6 @@ print(datasets.DESCR)
 print(datasets.feature_names)
 
 
-
 x = datasets['data']
 y = datasets['target']
 print(x.shape, y.shape) # (569, 30) (569,)
@@ -50,6 +49,7 @@ print('loss : ', loss)
 print('accuracy : ', loss)
 
 y_predict = model.predict(x_test)  #x_test를 넣었을때 y값을 예측할거다
+y_predict = np.asarray(y_predict, dtype = int)
 print(y_predict[:10])   #y 예측값 10개를 보자  = > 정수형으로 바꿔야 한다.
 print(y_test[:10])      #y 원래값 10개를 보자
 '''
@@ -66,11 +66,40 @@ print(y_test[:10])      #y 원래값 10개를 보자
 [1 0 1 1 0 1 1 1 0 1]
 print 결과 마이너스(실수)가 나왔다 정수로 바꿔야함.
 '''
-y_predict = y_predict.flatten()
-y_predict = np.where(y_predict > 0.5, 1 , 0)
- 
+
+
+
+
 from sklearn.metrics import accuracy_score
 # accuracy_score(y_test, y_predict)         # y테스트 값과  y예측값을 비교
 # print("accuracy_score : ", acc)
 
 
+
+
+
+
+                 
+
+
+
+'''         
+<정수로 바꾸기1>
+y_predict = model.predict(x_test)
+y_predict = np.asarray(y_predict, dtype = int)                 
+
+# np.asarray: 입력된 데이터를 np.array 형식으로 만듬.      
+                                          
+# dtype 속성: 데이터 형식 변경 
+(int: 정수형 / float: 실수형 / complex: 복소수형 / str: 문자형)
+'''   
+
+
+'''
+<정수로 바꾸기2>
+y_predict = y_predict.flatten()
+y_predict = np.where(y_predict > 0.5, 1 , 0)
+
+# preds_1d = y_predict.flatten() # 차원 펴주기
+# pred_class = np.where(preds_1d > 0.5, 2 , 1) #0.5보다크면 2, 작으면 1
+ '''
